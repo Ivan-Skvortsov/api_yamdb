dc966a3229398e742c70204d5a3a2f8a9fa44d3f
Ivan Skvortsov <pprofcheg@gmail.com>
2022-02-03 10:46:49 +0300

    created IsAdminOrReadOnly permission

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -10,5 +10,11 @@ class IsAdmin(permissions.BasePermission):
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
-    # TODO Skvortsov
-    pass
+    """Create/delete allowed to admins. Safe methods allowed to everyone."""
+    message = 'Нет прав доступа'
+
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        if request.user.is_superuser or request.user.role == 'admin':
+            return True
