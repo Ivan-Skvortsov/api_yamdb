5440408252b50789c66ebefa53f4443e33bcb0a1
Ivan Skvortsov <pprofcheg@gmail.com>
2022-02-03 17:56:40 +0300

    refactoring signals

--- api_yamdb/reviews/signals.py
+++ api_yamdb/reviews/signals.py
@@ -6,7 +6,8 @@ from reviews.models import Review
 
 
 @receiver([post_save, post_delete], sender=Review, dispatch_uid='title_rating')
-def update_title_rating(sender, instance, **kwargs):
+def update_title_rating(instance, **kwargs):
+    """Updates Title instance rating, when related Review instance changed."""
     title = instance.title
     avg_rating = title.reviews.all().aggregate(Avg('score'))
     title.rating = int(avg_rating['score__avg'])
