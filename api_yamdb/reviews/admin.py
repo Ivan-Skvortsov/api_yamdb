39474df4abb52bc43e0ea8aa01d62e478375af37
Ivan Skvortsov <pprofcheg@gmail.com>
2022-02-04 21:40:44 +0300

    registered reviews.models in admin

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from reviews.models import Genre, Title, Category, Review, Comment
 
-# Register your models here.
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'slug')
+    search_fields = ('name', 'slug')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'slug')
+    search_fields = ('name', 'slug')
+
+
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'year', 'rating', 'get_genres', 'category')
+    search_fields = ('name',)
+    list_filter = ('year', 'category')
+    list_editable = ('category',)
+    filter_horizontal = ('genre',)
+    empty_value_display = '-пусто-'
+
+    def get_genres(self, obj):
+        return "/".join([genre.name for genre in obj.genre.all()])
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('author', 'title', 'score', 'pub_date')
+    search_fields = ('title', 'author')
+    list_filter = ('score',)
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'review', 'pub_date')
